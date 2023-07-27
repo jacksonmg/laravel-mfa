767f6d86b97fa50c96ad592da0e64d3c45964181
Chris Brady <chris.brady@jacksonmg.com>
2023-07-27 17:23:40 -0400

    Formatting

--- config/laravel-mfa.php
+++ config/laravel-mfa.php
@@ -2,27 +2,27 @@
 
 return [
     'default' => [
-        'enabled' => env('MFA_ENABLED', true),
-        'template' => 'laravel-mfa::mfa.form',
+        'enabled'                   => env('MFA_ENABLED', true),
+        'template'                  => 'laravel-mfa::mfa.form',
         'code_expire_after_minutes' => 10,
-        'login_route' => 'login',  // Route name
+        'login_route'               => 'login',  // Route name
         /* Route after successfully verified.
            Uncomment if you want to define,
            otherwise app.url will be used. */
         // 'verified_route' => 'homepage',
-        'auth_user_closure' => function () {
+        'auth_user_closure'         => function () {
             return \Illuminate\Support\Facades\Auth::user();
         },
         'email' => [
-            'queue' => false, // If your app has a queue:work daemon, you can change it to true.
-            'template' => 'laravel-mfa::emails.authentication-code',
-            'subject' => 'Login authentication code',
+            'queue'     => false, // If your app has a queue:work daemon, you can change it to true.
+            'template'  => 'laravel-mfa::emails.authentication-code',
+            'subject'   => 'Login authentication code',
         ],
     ],
 
     'group' => [ // Example of override default configs
         'admin' => [ // Middleware: 'mfa:admin'
-            'login_route' => 'admin.login',
+            'login_route'       => 'admin.login',
             'auth_user_closure' => function () {
                 return \Encore\Admin\Facades\Admin::user();
             },
